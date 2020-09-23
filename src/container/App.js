52d65fc9c0c3ef95bc197ef8561f60e30388cc63
Guilherme Tupinambá Durães <40327476+TupzZ@users.noreply.github.com>
2020-09-22 21:37:57 -0300

    State

--- src/container/App.js
+++ src/container/App.js
@@ -4,6 +4,25 @@ import RealTime from '../components/RealTime/RealTime';
 import styles from './App.module.css';
 
 function App() {
+	const state = {
+		weatherIcons: [
+			'sun-alt',
+			'sunny',
+			'rainy-sunny',
+			'hail-rainy',
+			'rainy-thunder',
+			'rainy',
+			'rainy-night',
+			'windy-raining',
+			'windy-thunder-raining',
+			'night',
+			'full-night',
+			'windy-night',
+			'tornado',
+		],
+		estacoes: ['São Paulo/SP', 'Cuiabá/MT', 'Campo Grande/MS'],
+	};
+
 	return (
 		<div className='App'>
 			<Header />
